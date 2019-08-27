f0bf634b76bce11f31982b07c1d9403b5769e05f
Peter Farr <Peter@PrismaPhonic.com>
2019-08-27 04:51:16 -0700

    Updated domain events trait to have to return a data field which will be a simple hashmap for flexibility purposes

--- src/event.rs
+++ src/event.rs
@@ -1,13 +1,14 @@
 use chrono::Utc;
 use std::hash::Hash;
 use crate::models::Entity;
+use std::collections::HashMap;
 
 /// `DomainEvent` is a trait that defines an event relevant to the domain.  These are always facts about something
 /// that has already occurred that has domain significance.  An event has a time at which the event occurred,
 /// and an id that corresponds to which aggregate id the event corresponds to.  The implementor
 /// should also be sure to pass the aggregates `version` in when they create the event, so that
 /// events can be processed in the correct order.
-pub trait DomainEvent<'a, T: Hash + Eq> {
+pub trait DomainEvent<'a, T: Hash + Eq, V> {
     /// occurred should return the time the event occurred on.
     fn occurred(&self) -> chrono::DateTime<Utc>;
 
@@ -18,6 +19,12 @@ pub trait DomainEvent<'a, T: Hash + Eq> {
     /// version holds the version of the aggregate that the event corresponds to, which can be
     /// used to correctly order events for playback.
     fn version(&self) -> u64;
+
+    /// data returns a hashmap of the underlying event data.  This allows domain events to change over time.
+    /// If relevant event data was defined in the domain event's struct, then it would be tied to a specific type.
+    /// With this pattern we instead keep domain events more loosely coupled, so we aren't tied closely to the type
+    /// of the implementor for projections.
+    fn data(&self) -> HashMap<String, V>;
 }
 
 // TODO: Change from Entity to Aggregate after creating Aggregate trait.
