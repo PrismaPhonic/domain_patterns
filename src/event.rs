522ed654cfb48670a6a04ed334a5ef4a1f5b6be8
Peter Farr <Peter@PrismaPhonic.com>
2019-08-28 08:02:12 -0700

    Built out significant event logic that is now passing tests

--- src/event.rs
+++ src/event.rs
@@ -1,77 +1,58 @@
-use chrono::Utc;
-use std::hash::Hash;
-use crate::models::Entity;
-use std::collections::HashMap;
+use serde::Serialize;
+use crate::models::AggregateRoot;
+use uuid::Uuid;
 
 /// `DomainEvent` is a trait that defines an event relevant to the domain.  These are always facts about something
 /// that has already occurred that has domain significance.  An event has a time at which the event occurred,
 /// and an id that corresponds to which aggregate id the event corresponds to.  The implementor
 /// should also be sure to pass the aggregates `version` in when they create the event, so that
 /// events can be processed in the correct order.
-pub trait DomainEvent<'a, T: Hash + Eq, V> {
-    /// occurred should return the time the event occurred on.
-    fn occurred(&self) -> chrono::DateTime<Utc>;
+pub trait DomainEvent: Serialize {
+    /// occurred should return a timestamp as an i64.  You can generate this using a library like chrono
+    /// for the current time.
+    fn occurred(&self) -> i64;
+
+    /// id is the event's id, which should be automatically generated when constructing the implementor
+    /// of DomainEvent trait.  Returned value should be a clone (do not pass ownership)
+    fn id(&self) -> &Uuid;
 
     /// aggregate_id should correlate to the id of the aggregate pushing the event down
-    /// the event stream.
-    fn aggregate_id(&self) -> &'a T;
+    /// the event stream. Returned value should be a clone (do not pass ownership)
+    fn aggregate_id(&self) -> &Uuid;
 
     /// version holds the version of the aggregate that the event corresponds to, which can be
     /// used to correctly order events for playback.
     fn version(&self) -> u64;
-
-    /// data returns a hashmap of the underlying event data.  This allows domain events to change over time.
-    /// If relevant event data was defined in the domain event's struct, then it would be tied to a specific type.
-    /// With this pattern we instead keep domain events more loosely coupled, so we aren't tied closely to the type
-    /// of the implementor for projections.
-    fn data(&self) -> HashMap<String, V>;
 }
 
 // TODO: Change from Entity to Aggregate after creating Aggregate trait.
-// Note: This is highly subject to change.  Not suggested for public consumption yet.
-pub trait EventStorer<'b, T, U, V> where
-    T: 'b + Hash + Eq,
-    U: Entity<T>,
-    V: DomainEvent<'b, T>
+// Note #1: This is highly subject to change.  Not suggested for public consumption yet.
+// Note #2: Treat this like a repository interface for events per resource.  For instance, an
+// implementor might be a UserEventStore that stores all user related events, necessary for re-constructing
+// a user aggregate from an event stream.
+//
+// Note #3: The implementor should hold a function for type casting event data to correct event type.
+pub trait EventStorer<T: AggregateRoot>
 {
-    /// all_events will return all events in the implementor of EventStorer.  This should not be
-    /// used directly, and is only for use by other trait methods.
-    fn all_events(&self) -> Vec<&V>;
-
     /// events_by_aggregate returns a vector of pointers to events filtered by the supplied
     /// aggregate id.
-    fn events_by_aggregate(&self, aggregate_id: &T) -> Vec<&V> {
-        self.all_events().into_iter().filter(|e|{
-            e.aggregate_id() == aggregate_id
-        }).collect()
-    }
+    fn events_by_aggregate(&self, aggregate_id: &Uuid) -> Vec<T::Events>;
 
     /// events_since_version will give the caller all the events that have occurred for the given
     /// aggregate id since the version number supplied.
-    fn events_since_version(&self, aggregate_id: &T, version: u64) -> Vec<&V> {
-        let mut events: Vec<&V> = self.all_events().into_iter().filter(|e|{
-            e.aggregate_id() == aggregate_id && e.version() > version
-        }).collect();
-
-        events.sort_by(|a, b| a.version().cmp(&b.version()));
-
-        events
-    }
+    fn events_since_version(&self, aggregate_id: &Uuid, version: u64) -> Vec<T::Events>;
 
     // num_events_since_version provides a vector of events of a length equal to the supplied `num_events`
     // integer, starting from version + 1, and going up to version + num_events in sequential order.
     //
     // Used for re-hydrating aggregates, where the aggregate root can ask for chunks of events that occurred
     // after it's current version number.
-    fn num_events_since_version(&self, aggregate_id: &T, version: u64, num_events: u64) -> Vec<&V> {
-        let mut events: Vec<&V> = self.all_events().into_iter().filter(|e|{
-            e.aggregate_id() == aggregate_id &&
-                e.version() > version &&
-                e.version() <= version + num_events
-        }).collect();
-
-        events.sort_by(|a, b| a.version().cmp(&b.version()));
-
-        events
-    }
+    fn num_events_since_version(&self, aggregate_id: &Uuid, version: u64, num_events: u64) -> Vec<T::Events>;
 }
+
+//// EventApplier should be applied only to aggregate roots in systems where you want to use event sourcing.
+//pub trait EventApplier<T: DomainEvent>: AggregateRoot {
+//    type Error;
+//
+//    fn apply(event: T) -> Result<(), Self::Error>;
+//}
