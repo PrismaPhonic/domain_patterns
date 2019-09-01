86b116346597a225390ed0debe219cbf3c1b5406
Peter Farr <Peter@PrismaPhonic.com>
2019-09-01 14:06:38 -0700

    Added a read only repository, which will be useful in cases where one is using CQRS, or wants thin abstraction over read access into event sourced aggregates

--- src/collections.rs
+++ src/collections.rs
@@ -87,6 +87,45 @@ pub trait Repository<T: AggregateRoot> {
     fn remove(&mut self, key: &Uuid) -> Result<Option<T>, Self::Error>;
 }
 
+/// A trait that provides a collection like abstraction over read only database access.
+///
+/// Generic `T` is some struct that implements `Entity<K>` where `K` is used as the key in the repository methods.  In other words
+/// it's expected that an entities id is used as the key for insert and retrieval.
+pub trait ReadRepository<T: AggregateRoot> {
+    /// The implementer of this trait must point this type at some sort of `Error`.  This `Error` should communicate that there was some
+    /// kind of problem related to communication with the underlying database.
+    type Error: Error;
+
+    /// Returns the entity corresponding to the supplied key as an owned type.
+    ///
+    /// # Failure case
+    ///
+    /// If we fail to communicate with the underlying storage, then an error is returned.
+    fn get(&self, key: &Uuid) -> Result<Option<T>, Self::Error>;
+
+
+    /// Returns a `Vec<T>` of entities, based on the supplied `page_num` and `page_size`.
+    /// The page_num should start at 1, but is up to the implementer to design as they see fit.
+    ///
+    /// # Failure case
+    ///
+    /// If we fail to communicate with the underlying storage, then an error is returned.
+    fn get_paged(&self, page_num: usize, page_size: usize) -> Result<Vec<T>, Self::Error>;
+
+    /// Returns `true` if the underlying storage contains an entity at the specified key,
+    /// and otherwise returns `false`.
+    ///
+    /// # Failure case
+    ///
+    /// If we fail to communicate with the underlying storage, then an error is returned.
+    ///
+    /// [`Eq`]: https://doc.rust-lang.org/std/cmp/trait.Eq.html
+    /// [`Hash`]: https://doc.rust-lang.org/std/hash/trait.Hash.html
+    fn contains_key(&self, key: &Uuid) -> Result<bool, Self::Error> {
+        Ok(self.get(key)?.is_some())
+    }
+}
+
 /// EventRepository is a trait that provides collection like semantics over event storage and retrival.  The
 /// implementor may choose to persist and retrieve events from any storage mechanism of their choosing.
 pub trait EventRepository {
