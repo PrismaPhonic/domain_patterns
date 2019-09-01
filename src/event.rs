5dc0d3ff5d414e4842f0a94a7faa45bc02a20355
Peter Farr <Peter@PrismaPhonic.com>
2019-09-01 12:55:24 -0700

    Significant work on event repository trait

--- src/event.rs
+++ src/event.rs
@@ -28,32 +28,42 @@ pub trait DomainEvent: Serialize {
 /// DomainEvents is a thin wrapper over an enum that contains all generics that implement `DomainEvent` trait.
 /// As far as I can tell there's no way to constrain that all variants of an enum enforce the same trait.
 ///
-/// Note: This should be implemented by the AggregateRoot.
+/// Note: This should be implemented on an enum once for every aggregate root.
 pub trait DomainEvents {}
 
-// TODO: Change from Entity to Aggregate after creating Aggregate trait.
-// Note #1: This is highly subject to change.  Not suggested for public consumption yet.
-// Note #2: Treat this like a repository interface for events per resource.  For instance, an
-// implementor might be a UserEventStore that stores all user related events, necessary for re-constructing
-// a user aggregate from an event stream.
-//
-// Note #3: The implementor should hold a function for type casting event data to correct event type.
-pub trait EventStorer {
+/// EventRepository is a trait that provides collection like semantics over event storage and retrival.  The
+/// implementor may choose to persist and retrieve events from any storage mechanism of their choosing.
+pub trait EventRepository {
     type Events: DomainEvents;
     /// events_by_aggregate returns a vector of pointers to events filtered by the supplied
     /// aggregate id.
-    fn events_by_aggregate(&self, aggregate_id: &Uuid) -> Vec<Self::Events>;
+    fn events_by_aggregate(&self, aggregate_id: &Uuid) -> Option<Vec<Self::Events>>;
 
     /// events_since_version will give the caller all the events that have occurred for the given
     /// aggregate id since the version number supplied.
-    fn events_since_version(&self, aggregate_id: &Uuid, version: u64) -> Vec<Self::Events>;
+    fn events_since_version(&self, aggregate_id: &Uuid, version: u64) -> Option<Vec<Self::Events>>;
 
-    // num_events_since_version provides a vector of events of a length equal to the supplied `num_events`
-    // integer, starting from version + 1, and going up to version + num_events in sequential order.
-    //
-    // Used for re-hydrating aggregates, where the aggregate root can ask for chunks of events that occurred
-    // after it's current version number.
-    fn num_events_since_version(&self, aggregate_id: &Uuid, version: u64, num_events: u64) -> Vec<Self::Events>;
+    /// num_events_since_version provides a vector of events of a length equal to the supplied `num_events`
+    /// integer, starting from version + 1, and going up to version + num_events in sequential order.
+    ///
+    /// Used for re-hydrating aggregates, where the aggregate root can ask for chunks of events that occurred
+    /// after it's current version number.
+    fn num_events_since_version(&self, aggregate_id: &Uuid, version: u64, num_events: u64) -> Option<Vec<Self::Events>>;
+
+
+    /// Returns the event if it exists that corresponds to the supplied event_id as an owned type.
+    fn get(&self, event_id: &Uuid) -> Option<Self::Events>;
+
+    /// Returns a boolean indicating whether the event repository contains the event by the supplied id.
+    fn contains_event(&self, event_id: &Uuid) -> bool {
+        self.get(event_id).is_some()
+    }
+
+    /// Returns a bool letting the caller know if the event repository contains any events associated with the aggregate id.
+    fn contains_aggregate(&self, aggregate_id: &Uuid) -> bool;
+
+    /// Inserts a new domain event into the event store.
+    fn insert(&mut self, event: &Self::Events) -> Option<Self::Events>;
 }
 
 //// EventApplier should be applied only to aggregate roots in systems where you want to use event sourcing.
