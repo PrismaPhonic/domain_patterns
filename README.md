48a50a2a8f57a5072bb0b0ced7f433f21f22cde4
Peter Farr <Peter@PrismaPhonic.com>
2019-09-03 13:27:04 -0700

    Updated code comments and docs

--- README.md
+++ README.md
@@ -21,3 +21,101 @@ struct User {
     version: u64
 };
 ```
+
+## ValueSetup macro
+The `ValueSetup` derive macro can be used to setup as much boilerplate as possible
+for your choosen value object.  It checks some preconditions:
+
+1. You are applying this to a struct.
+2. Your struct has a single field called `value` of any type that is clonable.
+
+Once you've used this macro, you will still need to implement the `ValueObject` trait,
+but you will not have to implement `TryFrom` (or create the validation error for `TryFrom`, this
+is handled by the macro), or implement `PartialEq` or `Clone`.
+
+In case you need to use the validation error elsewhere, the created validation error will be the
+name of your struct with ValidationError appended.  For example, if you have an `Email` struct,
+then the generated validation error will be called `EmailValidationError`.
+
+```edition2018
+#[macro_use]
+extern crate domain_derive;
+
+use domain_patterns::ValueObject;
+use regex::Regex;
+
+#[derive(ValueSetup)]
+pub struct Email {
+    pub value: String,
+}
+
+impl ValueObject<String> for Email {
+    fn validate(value: &String) -> bool {
+        let email_rx = Regex::new(
+            r"^(?i)[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$"
+        ).unwrap();
+
+        email_rx.is_match(value)
+    }
+
+    fn value(&self) -> &String {
+        return &self.value;
+    }
+}
+```
+
+## DomainEvent macro
+The `DomainEvent` macro should be applied to a struct that represents a DomainEvent. It completely
+implements all methods of the `DomainEvent` trait, as long as some preconditions are met:
+
+1. You are applying this to a struct.
+2. There needs to be an `id` field of type `Uuid`.
+3. There needs to be a version field of any integer type (floating point not allowed).
+4. There needs to be an `aggregate_id` field of type `Uuid`.
+5. There needs to be an `occurred` field of type `i64`.
+
+```edition2018
+#[macro_use]
+extern crate domain_derive;
+
+use uuid::Uuid;
+use domain_patterns::event::DomainEvent;
+
+#[derive(Serialize, Clone, DomainEvent)]
+pub struct FirstNameUpdatedEvent {
+    pub aggregate_id: Uuid,
+    pub first_name: String,
+    pub version: u64,
+    pub id: Uuid,
+    pub occurred: i64,
+}
+```
+
+## DomainEvents macro
+The `DomainEvents` macro should be applied to an enum that holds variants which are all Domain Events.
+This is a very thin wrapper, and all the macro does is check that the structure is an enum, and then applies
+the trait, which has no methods.
+
+```edition2018
+#[macro_use]
+extern crate domain_derive;
+
+use uuid::Uuid;
+use domain_patterns::event::{DomainEvent, DomainEvents};
+
+#[derive(Serialize, Clone, DomainEvent)]
+pub struct FirstNameUpdatedEvent {
+    pub aggregate_id: Uuid,
+    pub first_name: String,
+    pub version: u64,
+    pub id: Uuid,
+    pub occurred: i64,
+}
+
+#[derive(Clone, DomainEvents)]
+pub enum UserEvents {
+    FirstNameUpdated(FirstNameUpdatedEvent),
+}
+```
+
+License: MIT
--- src/lib.rs
+++ src/lib.rs
@@ -57,8 +57,62 @@
 //!     }
 //!
 //!     fn value(&self) -> &String {
-//!         return &self.value
+//!         return &self.value;
 //!     }
+//! }
+//! ```
+//!
+//! # DomainEvent macro
+//! The `DomainEvent` macro should be applied to a struct that represents a DomainEvent. It completely
+//! implements all methods of the `DomainEvent` trait, as long as some preconditions are met:
+//!
+//! 1. You are applying this to a struct.
+//! 2. There needs to be an `id` field of type `Uuid`.
+//! 3. There needs to be a version field of any integer type (floating point not allowed).
+//! 4. There needs to be an `aggregate_id` field of type `Uuid`.
+//! 5. There needs to be an `occurred` field of type `i64`.
+//!
+//! ```edition2018
+//! #[macro_use]
+//! extern crate domain_derive;
+//!
+//! use uuid::Uuid;
+//! use domain_patterns::event::DomainEvent;
+//!
+//! #[derive(Serialize, Clone, DomainEvent)]
+//! pub struct FirstNameUpdatedEvent {
+//!     pub aggregate_id: Uuid,
+//!     pub first_name: String,
+//!     pub version: u64,
+//!     pub id: Uuid,
+//!     pub occurred: i64,
+//! }
+//! ```
+//!
+//! # DomainEvents macro
+//! The `DomainEvents` macro should be applied to an enum that holds variants which are all Domain Events.
+//! This is a very thin wrapper, and all the macro does is check that the structure is an enum, and then applies
+//! the trait, which has no methods.
+//!
+//! ```edition2018
+//! #[macro_use]
+//! extern crate domain_derive;
+//!
+//! use uuid::Uuid;
+//! use domain_patterns::event::{DomainEvent, DomainEvents};
+//!
+//! #[derive(Serialize, Clone, DomainEvent)]
+//! pub struct FirstNameUpdatedEvent {
+//!     pub aggregate_id: Uuid,
+//!     pub first_name: String,
+//!     pub version: u64,
+//!     pub id: Uuid,
+//!     pub occurred: i64,
+//! }
+//!
+//! #[derive(Clone, DomainEvents)]
+//! pub enum UserEvents {
+//!     FirstNameUpdated(FirstNameUpdatedEvent),
 //! }
 //! ```
 
@@ -80,6 +134,24 @@ use crate::proc_macro::TokenStream;
 use syn::DeriveInput;
 use syn::spanned::Spanned;
 
+/// The `Entity` derive macro can be used to automatically implement all methods of the `Entity` trait
+/// from the `domain_patterns` crate.  This only works if certain preconditions are met:
+///
+/// 1. You are applying this to a struct.
+/// 2. Your struct has an `id` field of type `Uuid`.
+/// 3. Your struct has a `version` field which is some integer type.
+///
+/// ```edition2018
+/// #[macro_use]
+/// extern crate domain_derive;
+/// use uuid::Uuid;
+///
+/// #[derive(Entity)]
+/// struct User {
+///     id: Uuid,
+///     version: u64
+/// };
+/// ```
 #[proc_macro_derive(Entity)]
 pub fn entity_derive(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input as DeriveInput);
@@ -104,6 +176,46 @@ pub fn entity_derive(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
+/// The `ValueSetup` derive macro can be used to setup as much boilerplate as possible
+/// for your choosen value object.  It checks some preconditions:
+///
+/// 1. You are applying this to a struct.
+/// 2. Your struct has a single field called `value` of any type that is clonable.
+///
+/// Once you've used this macro, you will still need to implement the `ValueObject` trait,
+/// but you will not have to implement `TryFrom` (or create the validation error for `TryFrom`, this
+/// is handled by the macro), or implement `PartialEq` or `Clone`.
+///
+/// In case you need to use the validation error elsewhere, the created validation error will be the
+/// name of your struct with ValidationError appended.  For example, if you have an `Email` struct,
+/// then the generated validation error will be called `EmailValidationError`.
+///
+/// ```edition2018
+/// #[macro_use]
+/// extern crate domain_derive;
+///
+/// use domain_patterns::ValueObject;
+/// use regex::Regex;
+///
+/// #[derive(ValueSetup)]
+/// pub struct Email {
+///     pub value: String,
+/// }
+///
+/// impl ValueObject<String> for Email {
+///     fn validate(value: &String) -> bool {
+///         let email_rx = Regex::new(
+///             r"^(?i)[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$"
+///         ).unwrap();
+///
+///         email_rx.is_match(value)
+///     }
+///
+///     fn value(&self) -> &String {
+///         return &self.value;
+///     }
+/// }
+/// ```
 #[proc_macro_derive(ValueSetup)]
 pub fn value_object_derive(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input as DeriveInput);
@@ -173,6 +285,31 @@ pub fn value_object_derive(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
+/// The `DomainEvent` macro should be applied to a struct that represents a DomainEvent. It completely
+/// implements all methods of the `DomainEvent` trait, as long as some preconditions are met:
+///
+/// 1. You are applying this to a struct.
+/// 2. There needs to be an `id` field of type `Uuid`.
+/// 3. There needs to be a version field of any integer type (floating point not allowed).
+/// 4. There needs to be an `aggregate_id` field of type `Uuid`.
+/// 5. There needs to be an `occurred` field of type `i64`.
+///
+/// ```edition2018
+/// #[macro_use]
+/// extern crate domain_derive;
+///
+/// use uuid::Uuid;
+/// use domain_patterns::event::DomainEvent;
+///
+/// #[derive(Serialize, Clone, DomainEvent)]
+/// pub struct FirstNameUpdatedEvent {
+///     pub aggregate_id: Uuid,
+///     pub first_name: String,
+///     pub version: u64,
+///     pub id: Uuid,
+///     pub occurred: i64,
+/// }
+/// ```
 #[proc_macro_derive(DomainEvent)]
 pub fn domain_event_derive(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input as DeriveInput);
@@ -205,6 +342,31 @@ pub fn domain_event_derive(input: TokenStream) -> TokenStream {
     TokenStream::from(expanded)
 }
 
+/// The `DomainEvents` macro should be applied to an enum that holds variants which are all Domain Events.
+/// This is a very thin wrapper, and all the macro does is check that the structure is an enum, and then applies
+/// the trait, which has no methods.
+///
+/// ```edition2018
+/// #[macro_use]
+/// extern crate domain_derive;
+///
+/// use uuid::Uuid;
+/// use domain_patterns::event::{DomainEvent, DomainEvents};
+///
+/// #[derive(Serialize, Clone, DomainEvent)]
+/// pub struct FirstNameUpdatedEvent {
+///     pub aggregate_id: Uuid,
+///     pub first_name: String,
+///     pub version: u64,
+///     pub id: Uuid,
+///     pub occurred: i64,
+/// }
+///
+/// #[derive(Clone, DomainEvents)]
+/// pub enum UserEvents {
+///     FirstNameUpdated(FirstNameUpdatedEvent),
+/// }
+/// ```
 #[proc_macro_derive(DomainEvents)]
 pub fn domain_events_derive(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input as DeriveInput);
