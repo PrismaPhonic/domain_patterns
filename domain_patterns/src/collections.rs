0019cd6bb5a7f05739395d1b70bc61fdfd6cf38e
Peter Farr <Peter@PrismaPhonic.com>
2019-09-15 17:05:56 -0700

    Changed read repo to match patterns of write repo exactly for maximum compatibility

--- domain_patterns/src/collections.rs
+++ domain_patterns/src/collections.rs
@@ -93,14 +93,14 @@ pub trait Repository<T: AggregateRoot> {
 /// it's expected that an entities id is used as the key for insert and retrieval.
 pub trait ReadRepository<T: AggregateRoot> {
     /// An error that communicates that something went wrong at the database level.
-    type Error: std::error::Error + std::fmt::Display;
+    type Error: std::error::Error + std::fmt::Display + 'static;
 
     /// Returns the entity corresponding to the supplied key as an owned type.
     ///
     /// # Failure case
     ///
     /// If we fail to communicate with the underlying storage, then an error is returned.
-    fn get(&self, key: &String) -> Result<Option<T>, Self::Error>;
+    fn get(&mut self, key: &String) -> Result<Option<T>, Self::Error>;
 
 
     /// Returns a `Vec<T>` of entities, based on the supplied `page_num` and `page_size`.
@@ -109,7 +109,7 @@ pub trait ReadRepository<T: AggregateRoot> {
     /// # Failure case
     ///
     /// If we fail to communicate with the underlying storage, then an error is returned.
-    fn get_paged(&self, page_num: usize, page_size: usize) -> Result<Vec<T>, Self::Error>;
+    fn get_paged(&mut self, page_num: usize, page_size: usize) -> Result<Vec<T>, Self::Error>;
 
     /// Returns `true` if the underlying storage contains an entity at the specified key,
     /// and otherwise returns `false`.
@@ -120,7 +120,7 @@ pub trait ReadRepository<T: AggregateRoot> {
     ///
     /// [`Eq`]: https://doc.rust-lang.org/std/cmp/trait.Eq.html
     /// [`Hash`]: https://doc.rust-lang.org/std/hash/trait.Hash.html
-    fn contains_key(&self, key: &String) -> Result<bool, Self::Error> {
+    fn contains_key(&mut self, key: &String) -> Result<bool, Self::Error> {
         Ok(self.get(key)?.is_some())
     }
 }
